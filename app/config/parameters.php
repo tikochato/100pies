91321200fefa546a5f7fa30db6783e096c3b6e68
Tikochato <daor01@gmail.com>
2018-03-19 17:38:24 -0600

    Iniciando sitio

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'id5073359_100pies',
+    'database_user' => 'id5073359_daor',
+    'database_password' => 'C5WRp9w8wQpYKg2k',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'KrXkEx8KEbTcppjwjIOVTyfnDHmNEsc669FG7FLRyZryfLNZEFZufejW',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-03-14',
+    'locale' => 'es-ES',
+    'cookie_key' => 'B7x1yZjsoQcF3Ab7EhwJQstWUyegoeHN8tOS5wgVgIEP3LjgPogDinq7',
+    'cookie_iv' => 'ePTfuwif',
+    'new_cookie_key' => 'def00000f8e2c5cd452a4115d4422703503e109446d5026d8fcfc7241a6a8edd95796ad84b57a698c93fc63712d4da5f4b1603bf3c926a9ae4fbabd1027c75a9899c38ca',
+  ),
+);
